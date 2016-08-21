e6bc9ce387ad720dc3d12f99ee2763f63281fcea
Yong <hoff@163.com>
2016-08-21 19:05:35 -0400

    initial version
    Signed-off-by: Yong <hoff@163.com>

---
+++ router/router.js
@@ -0,0 +1,13 @@
+﻿var url = require("url");
+
+var testPromise = require("../controller/testPromise");
+
+function route(req, res) {
+    var pathname = url.parse(req.url).pathname;
+    console.log("About to route a request for " + pathname);
+    if (pathname.startsWith("/testPromise")) {
+        testPromise.TestPromise();
+    }
+}
+
+exports.route = route;
---
+++ server.js
@@ -0,0 +1,10 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+var router = require("./router/router");
+
+http.createServer(function (req, res) {
+    router.route(req, res);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+
